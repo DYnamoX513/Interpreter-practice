d3e10cd3d0ca20ef84f74db88d17370251264136
Duyx <923948485@qq.com>
2020-02-21 14:48:56 +0800

    Initial commit（the Final Edition when the curriculum was finished）

---
+++ headers/PreDefined.h
@@ -0,0 +1,15 @@
+//
+// Created by 段宇昕 on 2019/12/9.
+//
+
+#ifndef GRAMMAR_PREDEFINED_H
+#define GRAMMAR_PREDEFINED_H
+
+#include <string>
+using namespace std;
+const string preDefinedFunc[] ={ // NOLINT(cert-err58-cpp)
+        "print",
+        "scan"
+};
+
+#endif //GRAMMAR_PREDEFINED_H
